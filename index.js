3bad72ab33736c78bd372743831b0a96c6d5f8ad
Salsedini <d.s.022222@gmail.com>
2021-11-03 17:14:54 +0100

    jagger redbull

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+function getRandomInt() {
+    return Math.floor(Math.random() * (4 - 1)) + 1;
+  }
+
+
 const alcoholics =  ["Barcelo","Larios","Jagger","Absolut","Negrita","Fireball","Malibu","Legendario","Bacardi"]
 const mixes = ["Cocacola", "Fanta Naranja", "Redbull", "Nestea", "Fanta Limon", "Aquarius", "7up"]
 const random = ["Sin hielo", ""]
+
+switch (getRandomInt()) {
+    case 1:
+      //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1
+      break;
+    case 2:
+      //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor2
+      break;
+    
+    case 3:
+      //Declaraciones ejecutadas cuando el resultado de expresión coincide con valorN
+      break;
+    default:
+      //Declaraciones ejecutadas cuando ninguno de los valores coincide con el valor de la expresión
+      break;
+  }
