19491d40743ea146c257098949887a416f2ea641
Salsedini <d.s.022222@gmail.com>
2021-11-03 17:35:52 +0100

    Probablemente mi tfg

--- index.js
+++ index.js
@@ -6,17 +6,20 @@ function getRandomInt(min, max) {
 const alcoholics =  ["Barcelo","Larios","Jagger","Absolut","Negrita","Fireball","Malibu","Legendario","Bacardi"]
 const mixes = ["Cocacola", "Fanta Naranja", "Redbull", "Nestea", "Fanta Limon", "Aquarius", "7up"]
 const random = ["Sin hielo", ""]
-const cerveza
+const cerveza = ["caña", "tercio", "jarra"]
 
 switch (getRandomInt(1,4)) {
     case 1:
-      //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1
+      //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1      
+      console.log(`Te vas a librar, sólo te pides una ${cerveza[getRandomInt(0,3)]}.\n`)
       break;
     case 2:
+        console.log(`Mala suerte, javibu te pilla por banda y te invita a un chupito de ${alcoholics[getRandomInt(0,9)]}.\n`)
       //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor2
       break;
     
     case 3:
+        console.log(`Vas a verte obligado a pedirte un cubata de ${alcoholics[getRandomInt(0,9)]} con ${mixes[getRandomInt(0,7)]}`)
       //Declaraciones ejecutadas cuando el resultado de expresión coincide con valorN
       break;
     default:
