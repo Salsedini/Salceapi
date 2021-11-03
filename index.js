bb38194f71350ac426a1c7b9b23b180b8955d748
leivaa <adrianleiva212@gmail.com>
2021-11-03 17:07:37 +0100

    alcoholics mixes

---
+++ index.js
@@ -0,0 +1,3 @@
+const alcoholics =  ["Barcelo","Larios","Jagger","Absolut","Negrita","Fireball","Malibu","Legendario","Bacardi"]
+const mixes = ["Cocacola", "Fanta Naranja", "Redbull", "Nestea", "Fanta Limon", "Aquarius", "7up"]
+const random = ["Sin hielo", ""]
