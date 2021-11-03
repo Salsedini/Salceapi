ed03b5f19f89443ad19bb6c3becf1613e5a74999
Salsedini <d.s.022222@gmail.com>
2021-11-03 17:17:53 +0100

    Calvo hazte un trompo

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-function getRandomInt() {
-    return Math.floor(Math.random() * (4 - 1)) + 1;
-  }
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
 
 const alcoholics =  ["Barcelo","Larios","Jagger","Absolut","Negrita","Fireball","Malibu","Legendario","Bacardi"]
 const mixes = ["Cocacola", "Fanta Naranja", "Redbull", "Nestea", "Fanta Limon", "Aquarius", "7up"]
 const random = ["Sin hielo", ""]
+const cerveza
 
-switch (getRandomInt()) {
+switch (getRandomInt(1,4)) {
     case 1:
       //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1
       break;
