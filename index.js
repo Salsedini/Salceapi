a6755caf1ff52de056d9c33ed4e6e9d8cfa5641d
Salsedini <d.s.022222@gmail.com>
2021-11-04 12:31:15 +0100

    Zumito siempre

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function getRandomInt(min, max) {
 
 
 const alcoholics =  ["Barcelo","Larios","Jagger","Absolut","Negrita","Fireball","Malibu","Legendario","Bacardi"]
-const mixes = ["Cocacola", "Fanta Naranja", "Redbull", "Nestea", "Fanta Limon", "Aquarius", "7up"]
+const mixes = ["Cocacola", "Fanta Naranja", "Redbull", "Nestea", "Fanta Limon", "Aquarius", "7up", "Zumo piña"]
 const random = ["Sin hielo", ""]
 const cerveza = ["caña", "tercio", "jarra"]
 
